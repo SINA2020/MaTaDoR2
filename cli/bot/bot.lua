63400cfa46fd7dec1e4f478277e97560d6a2bc38
SINA2020 <shomareh.majazi.nice@gmail.com>
2017-11-02 07:43:58 +0330

    Update bot.lua

--- cli/bot/bot.lua
+++ cli/bot/bot.lua
@@ -97,10 +97,10 @@ function create_config( )
     disabled_channels = {},
     moderation = {data = './data/moderation.json'},
     info_text = [[
-	》MaTaDoR BoT v5.7
+	》Nice BoT v5.7
 An advanced administration bot based on https://valtman.name/telegram-cli
 
-》https://github.com/BeyondTeam/BDReborn 
+》https://telegram.me/nice20team 
 
 》Admins :
 》@shahin20nice ➣ Founder & Developer《
